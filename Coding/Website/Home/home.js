ca0bf4f087c7b18baee72b4f192839aef7dce8a4
AmyMSutherland <59186393+AmyMSutherland@users.noreply.github.com>
2020-10-22 21:04:11 -0400

    toggle dark and light mode

--- Coding/Website/Home/home.js
+++ Coding/Website/Home/home.js
@@ -8,6 +8,25 @@ function off() {
   console.log("off");
 }
 function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+  var element = document.getElementsByClassName("homepageBanner")[0];
+  console.log(element.classList);
+
+  if (element.classList.contains("dark-mode-wrapper")) {
+    element.classList.remove("dark-mode-wrapper");
+    element.classList.add("light-mode-wrapper");
+  } else if (element.classList.contains("light-mode-wrapper")) {
+    element.classList.remove("light-mode-wrapper");
+    element.classList.add("dark-mode-wrapper");
+  } else if (
+    !element.classList.contains("light-mode-wrapper") &&
+    !element.classList.contains("dark-mode-wrapper")
+  ) {
+    element.classList.add("dark-mode-wrapper");
+  }
+
+  console.log (element.classList);
+
+   element = document.getElementsById("nav_bar");
+
+
 } //Attempting for dark mode//
