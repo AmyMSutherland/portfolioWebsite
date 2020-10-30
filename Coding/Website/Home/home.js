ad54890b0d7d29e4a400d02d17ae9dc0a258cbcd
AmyMSutherland <59186393+AmyMSutherland@users.noreply.github.com>
2020-10-30 12:52:09 -0400

    getting closer to a workable dark mode
    working on darkmode

--- Coding/Website/Home/home.js
+++ Coding/Website/Home/home.js
@@ -22,11 +22,26 @@ function myFunction() {
     !element.classList.contains("dark-mode-wrapper")
   ) {
     element.classList.add("dark-mode-wrapper");
-  }
+  } //darkmode for  homepage banner//
 
   console.log (element.classList);
 
-   element = document.getElementsById("nav_bar");
+  var element = document.getElementsByClassName("container")[0];
+  console.log(element.classList);
 
+  
+   if (element.classList.contains("dark-mode-wrapper")) {
+    element.classList.remove("dark-mode-wrapper");
+    element.classList.add("light-mode-wrapper");
+  } else if (element.classList.contains("light-mode-wrapper")) {
+    element.classList.remove("light-mode-wrapper");
+    element.classList.add("dark-mode-wrapper");
+  } else if (
+    !element.classList.contains("light-mode-wrapper") &&
+    !element.classList.contains("dark-mode-wrapper")
+  ) {
+    element.classList.add("dark-mode-wrapper");
+  }
+  console.log (element.classList);
 
-} //Attempting for dark mode//
+} //dark mode for nav bar//
